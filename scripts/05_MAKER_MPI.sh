0774814217f39914cf1686b3695e56170b01ccae
Amo Ikiror <amoikiror@gmail.com>
2025-10-26 14:42:20 +0100

    alter directory paths

--- scripts/05_MAKER_MPI.sh
+++ scripts/05_MAKER_MPI.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=MPI
 #SBATCH --partition=pibu_el8 
-#SBATCH --cpus-per-task=4 
 #SBATCH --mem=120G 
 #SBATCH --nodes=1
 #SBATCH --time=7-0
@@ -12,26 +11,19 @@
 #SBATCH --mail-type=end
 
 #directories
-WORKDIR="/data/users/aikiror/genomeAnnotation"
-COURSEDIR="${WORKDIR}/CDS_annotation"
-REPEATMASKER_DIR="${COURSEDIR}/softwares/RepeatMasker"
-MAKER_FOLDER="${WORKDIR}/output/04_MAKER_control"
+WORKDIR="/data/users/aikiror/genomeAnnotation/output/04_MAKER_control"
 
-export PATH=$PATH:${REPEATMASKER_DIR}
+COURSEDIR="/data/users/aikiror/genomeAnnotation/CDS_annotation"
 
-OUTPUT_DIR="${WORKDIR}/05_MAKER_MPI"
+REPEATMASKER_DIR="/data/users/aikiror/genomeAnnotation/CDS_annotation/softwares/RepeatMasker"
 
-#make path to outdir if it doesnt exist
-mkdir -p $OUTPUT_DIR
+export PATH=$PATH:"/data/users/aikiror/genomeAnnotation/CDS_annotation/softwares/RepeatMasker"
 
 #load modules
 module load OpenMPI/4.1.1-GCC-10.3.0
 module load AUGUSTUS/3.4.0-foss-2021a
 
 mpiexec --oversubscribe -n 50 apptainer exec \
-    --bind $SCRATCH:/TMP --bind $COURSEDIR --bind $AUGUSTUS_CONFIG_PATH --bind $REPEATMASKER_DIR \
+    --bind $SCRATCH:/TMP --bind $COURSEDIR --bind /data --bind $AUGUSTUS_CONFIG_PATH --bind $REPEATMASKER_DIR \
     ${COURSEDIR}/containers/MAKER_3.01.03.sif \
-    maker -mpi --ignore_nfs_tmp -TMP /TMP ${MAKER_FOLDER}/maker_opts.ctl ${MAKER_FOLDER}/maker_bopts.ctl \
-    ${MAKER_FOLDER}/maker_evm.ctl ${MAKER_FOLDER}/maker_exe.ctl
-
-
+    maker -mpi --ignore_nfs_tmp -TMP /TMP ${WORKDIR}/maker_opts.ctl ${WORKDIR}/maker_bopts.ctl ${WORKDIR}/maker_evm.ctl ${WORKDIR}/maker_exe.ctl
